539bc353fdea177c497145dba1544aa793b5ff22
pavan <pavanvkumar9@gmail.com>
2020-11-02 12:57:33 +0530

    index updated

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome' });
+});
+
 module.exports = router;
